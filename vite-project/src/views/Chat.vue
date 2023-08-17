4e1b1981203e763459f537506fc155136beba518
unknown <andreevivan8891@gmail.com>
2023-08-17 17:05:36 +0300

    added chat interface vue

--- vite-project/src/views/Chat.vue
+++ vite-project/src/views/Chat.vue
@@ -2,5 +2,148 @@
   <main id="chat">
     <h1>Chat</h1>
     <p>This is chat page</p>
+
+    <!-- New block for sent messages -->
+    <div class="sent-messages">
+      <p
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="{ 'sent-message': message.sent, 'cancelled-message': !message.sent }"
+      >
+        {{ message.text }}
+      </p>
+    </div>
+
+    <div class="chat-container">
+      <div v-for="(message, index) in messages" :key="index" class="input-block">
+        <textarea
+          rows="5"
+          v-model="message.text"
+          placeholder="Enter your text here..."
+          @keyup.enter="sendData(index)"
+        ></textarea>
+        <div class="button-block">
+          <div class="button-group">
+            <button class="button send-button" @click="sendData(index)">Send</button>
+            <button class="button cancel-button" @click="cancel(index)">Cancel</button>
+          </div>
+        </div>
+        <p v-if="message.sent">Sent message: {{ message.text }}</p>
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: [
+        { text: 'How are you?', sent: false }
+        // Add more initial messages as needed
+      ]
+    };
+  },
+  methods: {
+    sendData(index) {
+      // Implement your logic for sending data
+      this.messages[index].sent = true;
+    },
+    cancel(index) {
+      // Implement your logic for canceling
+      this.messages[index].text = '';
+      this.messages[index].sent = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#chat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* Three times larger than actual content */
+}
+
+.sent-messages {
+  width: 80%;
+  max-width: 800px;
+  height: 90vh; /* Three times larger than main block */
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 20px;
+  overflow-y: auto;
+}
+
+.sent-message {
+  color: green; /* Sent message color */
+  margin-bottom: 5px;
+}
+
+.cancelled-message {
+  color: red; /* Canceled message color */
+  margin-bottom: 5px;
+}
+
+.chat-container {
+  width: 80%;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.input-block {
+  width: 100%;
+  padding: 10px;
+  margin-top: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.button-block {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+
+.button-group {
+  display: flex;
+  align-items: center;
+}
+
+.button {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+.send-button {
+  background-color: #4caf50;
+  color: white;
+  margin-right: 5px;
+}
+
+.cancel-button {
+  background-color: #f44336;
+  color: white;
+}
+
+textarea {
+  width: 100%;
+}
+
+p {
+  margin-top: 10px;
+}
+</style>
