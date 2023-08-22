5eeec758ac3e635e3dfd7bcd823c0c02974771b0
unknown <andreevivan8891@gmail.com>
2023-08-22 08:12:04 +0300

    some fixes

--- yandex/alice.go
+++ yandex/alice.go
@@ -9,17 +9,20 @@ var respNse *alice.Kit
 
 func SimpleSkill() {
 	updates := alice.ListenForWebhook("https://onviz-api.ru/yandex")
-	respNse.Resp.Text("hello")
-	updates.Loop(func(k alice.Kit) *alice.Response {
-		req, resp := k.Init()
-		resp.RandomText("one", "two", "three").
-			Button("one", "", false).
-			Button("отстань", "", false)
-		if req.IsNewSession() {
-			return resp.Text("привет")
-		}
-		return resp.Text(req.OriginalUtterance())
-	})
+
+	if updates != nil {
+		respNse.Resp.Text("hello")
+		updates.Loop(func(k alice.Kit) *alice.Response {
+			req, resp := k.Init()
+			resp.RandomText("one", "two", "three").
+				Button("one", "", false).
+				Button("device-open?", "", false)
+			if req.IsNewSession() {
+				return resp.Text("device-start?")
+			}
+			return resp.Text(req.OriginalUtterance())
+		})
+	}
 }
 
 func Alice(w http.ResponseWriter, r *http.Request) {
