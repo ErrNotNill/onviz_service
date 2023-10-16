6b14f833a207f25d3742adb7d3696536782d8c89
unknown <andreevivan8891@gmail.com>
2023-10-16 12:40:08 +0300

    fixed alice handler

--- service/yandex/alice.go
+++ service/yandex/alice.go
@@ -8,7 +8,7 @@ import (
 var respNse *alice.Kit
 
 func SimpleSkill() {
-	updates := alice.ListenForWebhook("https://onviz-api.ru/yandex")
+	updates := alice.ListenForWebhook("/hook")
 
 	if updates != nil {
 		respNse.Resp.Text("hello")
@@ -26,6 +26,9 @@ func SimpleSkill() {
 }
 
 func Alice(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 	respNse.Resp.Text("hello")
 	for {
