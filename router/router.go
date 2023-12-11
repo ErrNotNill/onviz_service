4bdb15cbd52bc3b197049f6f3f464c966d0ebd46
unknown <andreevivan8891@gmail.com>
2023-12-11 16:01:47 +0300

    some fixes

--- router/router.go
+++ router/router.go
@@ -22,47 +22,47 @@ func Router() {
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"Authorization", "Content-Type"},
 	})
-	http.Handle("/auth_page", c.Handler(http.HandlerFunc(repository2.AuthPage)))
-	http.Handle("/login_page", c.Handler(http.HandlerFunc(repository2.LoginPage)))
+	http.Handle("/api/auth_page", c.Handler(http.HandlerFunc(repository2.AuthPage)))
+	http.Handle("/api/login_page", c.Handler(http.HandlerFunc(repository2.LoginPage)))
 
 	//http.HandleFunc("/devices/:device_id", tuya2.GetDeviceNew)
-	http.HandleFunc("/v1.0/", yandex2.CheckConnection)
-	http.HandleFunc("/v1.0/user/devices", yandex2.CheckConnection) //todo get user devices GET
+	http.HandleFunc("/api/v1.0/", yandex2.CheckConnection)
+	http.HandleFunc("/api/v1.0/user/devices", yandex2.CheckConnection) //todo get user devices GET
 	//http.HandleFunc("/v1.0/user/devices/query", yandex2.InfoAboutDevicesState) //todo info about state devices POST
 	//http.HandleFunc("/v1.0/user/devices/action", yandex2.ChangeDevicesState)   //todo change state of devices POST
 
-	http.HandleFunc("/yandex/token", yandex2.AuthUserFromYandexToken)
+	http.HandleFunc("/api/yandex/token", yandex2.AuthUserFromYandexToken)
 
-	http.HandleFunc("/yandex/v1.0/user/devices", service.GetDeviceNew)
-	http.HandleFunc("/yandex/v1.0/user/unlink", service.UnlinkUser)
-	http.HandleFunc("/yandex/v1.0/user/devices/query", service.GetDevicesStatus)
-	http.HandleFunc("/yandex/v1.0/user/devices/action", service.GetDevicesStatusChanged)
+	http.HandleFunc("/api/yandex/v1.0/user/devices", service.GetDeviceNew)
+	http.HandleFunc("/api/yandex/v1.0/user/unlink", service.UnlinkUser)
+	http.HandleFunc("/api/yandex/v1.0/user/devices/query", service.GetDevicesStatus)
+	http.HandleFunc("/api/yandex/v1.0/user/devices/action", service.GetDevicesStatusChanged)
 
-	http.HandleFunc("/v1.0", yandex2.CheckConnectionYandex)
-	http.HandleFunc("/get_auth_token", repository2.GetAuthTokenYandex)
-	http.HandleFunc("/refresh_token", service.RefreshToken)
+	http.HandleFunc("/api/v1.0", yandex2.CheckConnectionYandex)
+	http.HandleFunc("/api/get_auth_token", repository2.GetAuthTokenYandex)
+	http.HandleFunc("/api/refresh_token", service.RefreshToken)
 
 	//http.HandleFunc("/", LEADS.TestStatus)
-	http.HandleFunc("/chat", chat.TestChat)
+	http.HandleFunc("/api/chat", chat.TestChat)
 	//http.HandleFunc("/tilda", TildaWebHooks)
 	//http.HandleFunc("/getListOfLines", GetListOfLines)
-	http.HandleFunc("/callback", VK.CallBack)
+	http.HandleFunc("/api/callback", VK.CallBack)
 	//http.HandleFunc("/parse", testHandleFunc)
 
-	http.HandleFunc("/leads", repository.LeadsAdd)
-	http.HandleFunc("/leads_list", repository.GetLeads)
-	http.HandleFunc("/dealer_deal", repository.DealerDealAdded)
+	http.HandleFunc("/api/leads", repository.LeadsAdd)
+	http.HandleFunc("/api/leads_list", repository.GetLeads)
+	http.HandleFunc("/api/dealer_deal", repository.DealerDealAdded)
 	http.HandleFunc("/api/leads_get", repository.GetLeadsAll)
 
-	http.HandleFunc("/bot", bot_bitrix2.BotBitrix)
+	http.HandleFunc("/api/bot", bot_bitrix2.BotBitrix)
 	//http.HandleFunc("/auth", bot_bitrix.CallbackHandler)
-	http.HandleFunc("/redir", bot_bitrix2.RedirectHandler)
+	http.HandleFunc("/api/redir", bot_bitrix2.RedirectHandler)
 
-	http.HandleFunc("/auth_tuya", service.AuthHandler)
-	http.HandleFunc("/get_token", service.GetTokenHandler)
+	http.HandleFunc("/api/auth_tuya", service.AuthHandler)
+	http.HandleFunc("/api/get_token", service.GetTokenHandler)
 	//http.HandleFunc("/refresh_token", tuya.RefreshTokenHandler)
 
-	http.HandleFunc("/text_collect", chat.GetTextCollectHandler)
-	http.HandleFunc("/iframe", addons.IframeHandler)
+	http.HandleFunc("/api/text_collect", chat.GetTextCollectHandler)
+	http.HandleFunc("/api/iframe", addons.IframeHandler)
 
 }
