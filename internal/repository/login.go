c093d0341b8f606cebf53cf47d4fe6858024ab94
unknown <andreevivan8891@gmail.com>
2023-12-22 16:40:11 +0300

    some fixes

--- internal/repository/login.go
+++ internal/repository/login.go
@@ -241,9 +241,9 @@ func TokenOauth(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("string(rdr First Query):>", string(rdrNew))
 
-	http.Redirect(w, r, "https://oauth.yandex.ru/authorize?response_type=code&client_id=4fed8408c435482b950afeb2d6e0f3cc", http.StatusOK)
+	http.Redirect(w, r, "https://oauth.yandex.ru/authorize?response_type=code&client_id=4fed8408c435482b950afeb2d6e0f3cc&client_secret=dbb4420ab51f41fc86a2dedd37d2302b", http.StatusFound)
 	time.Sleep(4 * time.Second)
-	http.Redirect(w, r, "https://oauth.yandex.ru/authorize?response_type=code&client_id=4fed8408c435482b950afeb2d6e0f3cc", http.StatusOK)
+	http.Redirect(w, r, "https://oauth.yandex.ru/authorize?response_type=code&client_id=4fed8408c435482b950afeb2d6e0f3cc&client_secret=dbb4420ab51f41fc86a2dedd37d2302b", http.StatusFound)
 	code := r.URL.Query().Get("code")
 	fmt.Println("CODE:???", code)
 	CodeAuth = code
