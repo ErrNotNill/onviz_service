c7c7ebbd6ce7cca15cd396632986069128a5b88c
unknown <andreevivan8891@gmail.com>
2023-12-25 11:32:15 +0300

    some fixes

--- vite-project/src/views/Login.vue
+++ vite-project/src/views/Login.vue
@@ -295,7 +295,7 @@ function signinUser() {
         // Login successful, get the token
         //showSidebar.value = true;
 
-         window.location.href = "https://oauth.yandex.ru/authorize"
+         window.location.href = "https://oauth.yandex.ru/authorize?client_id=4fed8408c435482b950afeb2d6e0f3cc"
 
         //window.location.href = "https://onviz-api.ru/api/token_oauth"
         //window.location.href = "https://onviz-api.ru/api/yandex/authorize"
